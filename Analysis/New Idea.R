4a8620f68e6ca49e66738a94728f0a847d6732e9
Kathryn Malchow <78643030+KathrynMalchow@users.noreply.github.com>
2022-04-04 17:00:53 +0200

    save!

--- Analysis/New Idea.R
+++ Analysis/New Idea.R
@@ -1,5 +1,5 @@
 
-pacman::p_load(remotes, readr, dplyr, tidyverse, quanteda, spacyr, reticulate, tidytext)
+pacman::p_load(remotes, readr, dplyr, tidyverse, quanteda, spacyr, reticulate, tidytext, stringr)
 
 #df
 
@@ -7,100 +7,183 @@ tib_DataManagement
 
 df_coll
 
-#collect dfs that contain most frequent (top 5) collocations of df
+#collect dfs that contain most frequent (top 5) collocations of df - cannot make function because order of key words matters
 
-df_1 = tib_DataManagement[4]
-df_1 = df_1 %>% 
-  filter(str_detect(reviews, c("user", "friendly")))
+dm_1 = tib_DataManagement[4] %>% 
+  filter(str_detect(reviews, "user")) %>% 
+  filter(str_detect(reviews, "friendly"))
 
 
 
-#run through sentistrength - make into .txt
+ #run through sentistrength - make into .txt
 
-write.table(df_1, 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/df_1.txt', col.names=TRUE, row.names = FALSE)
+write.table(dm_1, 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_1.txt', col.names=TRUE, row.names = FALSE)
 
 
 
 #send through sentistrength
 
 #after sentiscoring - back into df
-df_1_scored = read.table(file = 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/df_1+results.txt', sep="\t", quote="", comment.char="")
+dm_1_scored = read.table(file = 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_1_comb+results.txt', sep="\t", quote="", comment.char="")
 
 
 #keep only scores and scoring section
-df_1_scored = df_1_scored[3:5] 
+dm_1_scored = dm_1_scored[3:5] 
 #rename and remove fist row
-df_1_scored = rename(df_1_scored,
+dm_1_scored = rename(dm_1_scored,
                   Positive = V3,
                   Negative = V4,
                   Rational = V5)
 
-df_1_scored = df_1_scored[-c(1), ]
+dm_1_scored = dm_1_scored[-c(1), ]
 #make scores numeric
-df_1_scored = df_1_scored %>% 
+dm_1_scored = dm_1_scored %>% 
   mutate_at(vars(Positive, Negative), as.numeric)
 
 
 
 ### Average score of positive and negative? 
 
-mean(df_1_scored$Positive) #  2.333
-mean(df_1_scored$Negative) # -1.458
+mean(dm_1_scored$Positive) #  2.55
+mean(dm_1_scored$Negative) # -1.36
+
+### Average of Stars
+
+dm_1_star = tib_DataManagement %>% 
+  filter(str_detect(reviews, "user")) %>% 
+  filter(str_detect(reviews, "friendly"))
+
+mean(dm_1_star$stars) #3.82
+
+############################## function for average sentiments - working
+senti_func = function(df_scored){
+  #keep only scores and scoring section
+  df_scored = df_scored[3:5] 
+  #rename and remove fist row
+  df_scored = rename(df_scored,
+                       Positive = V3,
+                       Negative = V4,
+                       Rational = V5)
+  
+  df_scored = df_scored[-c(1), ]
+  #make scores numeric
+  df_scored = df_scored %>% 
+    mutate_at(vars(Positive, Negative), as.numeric)
+  print(mean(df_scored$Positive))
+  print(mean(df_scored$Negative))
+}
 
 
-#####send through sentiscore
+########################### function for stars - not working
+star_func = function(df, wa, wb){
+  star_df =  df %>%
+    filter(str_detect(reviews, "wa")) %>% 
+    filter(str_detect(reviews, "wb")) 
+  print(mean(star_df$stars))
+}
 
-#take only reviews
-dm_new = tib_DataManagement[4] # or maybe not remove names and shit
-#turn into .txt
-write.table(dm_new, 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_new.txt', sep = " ", dec = ".",
-            row.names = TRUE, col.names = TRUE)
-
-
-write.table(tib_DataManagement, 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/test.txt', sep = " ", row.names = FALSE, col.names = FALSE)
-
-
-#after sentiscoring - back into df
-senti_dm = read.table(file = 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_new+results.txt', sep="\t", quote="", comment.char="")
+##########################
 
 
+################### rain_fall
 
-#keep only scores and scoring section
-senti_dm = senti_dm[3:5] 
-#rename and remove fist row
-senti_dm = rename(senti_dm,
-    Positive = V3,
-    Negative = V4,
-    Rational = V5)
+# collect relevant reviews
+dm_2 = tib_DataManagement[4] %>% 
+  filter(str_detect(reviews, "rain")) %>% 
+  filter(str_detect(reviews, "fall"))
 
-senti_dm = senti_dm[-c(1), ]
-#make scores numeric
-senti_dm = senti_dm %>% 
-  mutate_at(vars(Positive, Negative), as.numeric)
+# make into .txt file
+write.table(dm_2, 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_2.txt', col.names=TRUE, row.names = FALSE)
 
+# back into df
+dm_2_scored = read.table(file = 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_2+results.txt', sep="\t", quote="", comment.char="")
 
-
-#####add new column for overall sentiscore
-senti_dm = senti_dm %>% 
-  mutate(score = Negative + Positive)
+# average sentiment function
+senti_func(dm_2_scored)
 
 
-##### combine with orgininal df
+# mean star rating 
+star = tib_DataManagement %>% 
+  filter(str_detect(reviews, "rain")) %>% 
+  filter(str_detect(reviews, "fall"))
+
+mean(star$stars)
+
+################## waste_time
+
+# collect relevant reviews
+dm_3 = tib_DataManagement[4] %>% 
+  filter(str_detect(reviews, "time")) %>%
+  filter(str_detect(reviews, "waste")) 
+  
+
+# make into .txt file
+write.table(dm_3, 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_3.txt', col.names=TRUE, row.names = FALSE)
+
+# back into df
+dm_3_scored = read.table(file = 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_3+results.txt', sep="\t", quote="", comment.char="")
+
+# average sentiment function
+senti_func(dm_3_scored)
+
+
+# mean star rating 
+star = tib_DataManagement %>% 
+  filter(str_detect(reviews, "time")) %>% 
+  filter(str_detect(reviews, "waste"))
+
+mean(star$stars)
 
 
 
-###make new positive and negative dfs
-negative_reviews = senti_dm %>% 
-  subset(score < 0)
+################### keep_track
 
-positive_reviews = senti_dm %>% 
-  subset(score > 0)
-
-
-###use parsing function on both dfs
-spacy_initialize()
+# collect relevant reviews
+dm_4 = tib_DataManagement[4] %>% 
+  filter(str_detect(reviews, "keep")) %>%
+  filter(str_detect(reviews, "track")) 
 
 
-parse_fun()
+# make into .txt file
+write.table(dm_4, 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_4.txt', col.names=TRUE, row.names = FALSE)
+
+# back into df
+dm_4_scored = read.table(file = 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_4+results.txt', sep="\t", quote="", comment.char="")
+
+# average sentiment function
+senti_func(dm_4_scored)
 
 
+# mean star rating 
+star = tib_DataManagement %>% 
+  filter(str_detect(reviews, "keep")) %>% 
+  filter(str_detect(reviews, "track"))
+
+mean(star$stars)
+
+
+################## free_version
+
+# collect relevant reviews
+dm_5 = tib_DataManagement[4] %>% 
+  filter(str_detect(reviews, "free")) %>%
+  filter(str_detect(reviews, "version")) 
+
+
+# make into .txt file
+write.table(dm_5, 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_5.txt', col.names=TRUE, row.names = FALSE)
+
+# back into df
+dm_5_scored = read.table(file = 'C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Analysis/dm_5+results.txt', sep="\t", quote="", comment.char="")
+
+# average sentiment function
+senti_func(dm_5_scored)
+
+
+# mean star rating 
+star = tib_DataManagement %>% 
+  filter(str_detect(reviews, "free")) %>% 
+  filter(str_detect(reviews, "version"))
+
+mean(star$stars)
+  
