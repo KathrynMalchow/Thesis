ca8c16cad0f51b4c807fc06c1bf6d11f16e2f549
Kathryn Malchow <78643030+KathrynMalchow@users.noreply.github.com>
2022-04-11 18:53:38 +0200

    save

--- Analysis/Topic_Modeling2.R
+++ Analysis/Topic_Modeling2.R
@@ -1,6 +1,8 @@
 #topic modeling 2
 
-pacman::p_load(quanteda, tidytext, tidyverse, dplyr, ggplot2, tidyr, textmineR, topicmodels, seededlda, ggthemes, remotes, modelsummary, formattable)
+pacman::p_load(quanteda, tidytext, tidyverse, dplyr, ggplot2, 
+               tidyr, textmineR, topicmodels, seededlda, ggthemes, remotes,
+                modelsummary, formattable, htmltools, webshot)
 
 
 #####tib_DataManagement
@@ -76,17 +78,33 @@ colnames(ten_topic_names_df)[colnames(ten_topic_names_df) == 'names'] = 'Terms'
 colnames(ten_topic_names_df)[colnames(ten_topic_names_df) == 'topic'] = 'Topic'
 
 #table
-formattable(ten_topic_names_df, align = c("l","l"), list(
+dm_tm_table = formattable(ten_topic_names_df, align = c("l","r"), list(
   `Topic` = formatter("span",
-      x ~ icontext(ifelse(x == "topic1" | x == "topic5" | x == "topic7" | x == "topic13", "star", ""), x))
-))
-                
+      style = x ~ ifelse(x == "topic1" | x == "topic5" | x == "topic7" | x == "topic13", style(font.weight = "bold"), NA),
+      x ~ icontext(ifelse(x == "topic1" | x == "topic5" | x == "topic7" | x == "topic13", "star", ""), x)),
+  `Terms` = formatter("span",
+                      style = x ~ ifelse(x == "phone, accept, screen, terms, button, android, past, totals, time, acres" 
+                              | x == "free, data, version, money, month, pay, tracking, worth, found, activities" 
+                              | x == "easy, farm, keeping, record, makes, farming, easier, tool, recommend, track" | 
+                               x == "accurate, measure, easy, measuring, features, gps, feature, measurement, ads, distance", style(font.weight = "bold"), NA)
+)))
 
+#export table function
+export_formattable = function(f, file, width = "100%", height = NULL, 
+                               background = "white", delay = 0.2)
+{
+  w <- as.htmlwidget(f, width = width, height = height)
+  path <- html_print(w, background = background, viewer = NULL)
+  url <- paste0("file:///", gsub("\\\\", "/", normalizePath(path)))
+  webshot(url,
+          file = file,
+          selector = ".formattable_widget",
+          delay = delay)
+}
 
-#ifelse(x == "topic5", "star", ""),
-                #ifelse(x == "topic7", "star", ""),
-                #ifelse(x == "topic13", "star", ""), x))))
-
+webshot::install_phantomjs()
+             
+export_formattable(dm_tm_table, "C:/Users/Kathryn/Desktop/Master_Thesis/Thesis_Analysis/Graphs_Tables/dm_table.png")
 
 
 ### making a graph
@@ -113,10 +131,14 @@ remotes::install_github("hrbrmstr/hrbrthemes")
 library(hrbrthemes)
 
 #color palette
-
+farm_colors = #a47c48 
+  #99c140 	
+  #ffce36 
+  #ee4035 	
+  #fff2cc
 
 #plot
-topics_dm %>%
+dm_chart = topics_dm %>%
   subset(topic == "topic7" | topic == "topic5" | topic == "topic1"| topic == "topic13") %>% 
   ggplot(aes(reorder(topic, gamma), gamma, label = names, fill =
                topic)) +
@@ -131,6 +153,11 @@ topics_dm %>%
        subtitle = "With the top words that contribute to each topic") +
   theme_minimal()+
   scale_fill_brewer(palette = "Dark2")
+
+
+
+
+
 
 
 
